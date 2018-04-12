4544031392a8d1f19fa7f28254bdd09e666632cd
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-13 02:18:09 +0300

    Preferences window drawing

---
+++ QuickPlayTool/PreferencesWindowItems.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace QuickPlayTool
+{
+    public static class PreferencesWindowItems
+    {
+        [PreferenceItem("Quick Play Tool")]
+        public static void DrawPreferences()
+        {
+            EditorGUILayout.BeginVertical();
+
+            EditorGUILayout.PrefixLabel("Project Preferences Save Path");
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.TextField("");
+            GUILayout.Button("...", EditorStyles.miniButton, GUILayout.Width(30));
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
