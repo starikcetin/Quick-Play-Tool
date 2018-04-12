20e8e2901376ffa5f7833c52b3b42b2373ed6e50
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-13 02:54:11 +0300

    Functioning preferences items for project preferences folder path and file name

--- QuickPlayTool/PreferencesWindowItems.cs
+++ QuickPlayTool/PreferencesWindowItems.cs
@@ -8,16 +8,29 @@ namespace QuickPlayTool
         [PreferenceItem("Quick Play Tool")]
         public static void DrawPreferences()
         {
+            var saveFolderPath = EditorPrefsHelper.ProjectSettingsSaveFolderPath;
+            var saveFileName = EditorPrefsHelper.ProjectSettingsSaveFileName;
+
             EditorGUILayout.BeginVertical();
 
-            EditorGUILayout.PrefixLabel("Project Preferences Save Path");
+            GUILayout.Label("Project Preferences Save File");
 
+            // folder
             EditorGUILayout.BeginHorizontal();
-            GUILayout.TextField("");
-            GUILayout.Button("...", EditorStyles.miniButton, GUILayout.Width(30));
+            GUILayout.Label("Folder", GUILayout.Width(50));
+            saveFolderPath = GUILayout.TextField(saveFolderPath, GUILayout.Width(300));
+            EditorGUILayout.EndHorizontal();
+
+            // file
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("File", GUILayout.Width(50));
+            saveFileName = GUILayout.TextField(saveFileName, GUILayout.Width(300));
             EditorGUILayout.EndHorizontal();
 
             EditorGUILayout.EndVertical();
+
+            EditorPrefsHelper.ProjectSettingsSaveFolderPath = saveFolderPath;
+            EditorPrefsHelper.ProjectSettingsSaveFileName = saveFileName;
         }
     }
 }
