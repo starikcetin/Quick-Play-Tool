55e8f321faa845b3042da335d4eb945dc9646e14
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-12 23:20:09 +0300

    Marked what is a project setting and what can remain as an editorprefs entry.

--- QuickPlayTool/EditorPrefsHelper.cs
+++ QuickPlayTool/EditorPrefsHelper.cs
@@ -7,54 +7,64 @@ namespace QuickPlayTool
 {
     public static class EditorPrefsHelper
     {
+
+
+
+        
+        // editor pref
         public static bool ShowPaths
         {
             get { return EditorPrefs.GetBool("QuickPlayTool.ShowPaths"); }
             set { EditorPrefs.SetBool("QuickPlayTool.ShowPaths", value); }
         }
 
+        // editor pref
         public static bool ShowAsUtility
         {
             get { return EditorPrefs.GetBool("QuickPlayTool.ShowAsUtility"); }
             set { EditorPrefs.SetBool("QuickPlayTool.ShowAsUtility", value); }
         }
 
+        // editor pref
         public static bool AllScenesFoldout
         {
             get { return EditorPrefs.GetBool("QuickPlayTool.AllScenesFoldout"); }
             set { EditorPrefs.SetBool("QuickPlayTool.AllScenesFoldout", value); }
         }
 
-        public static string QuickPlaySceneRelativePath
-        {
-            get { return EditorPrefs.GetString("QuickPlayTool.QuickPlaySceneRelativePath"); }
-            set { EditorPrefs.SetString("QuickPlayTool.QuickPlaySceneRelativePath", value); }
-        }
-
+        // editor pref
         public static bool AutoCompact
         {
             get { return EditorPrefs.GetBool("QuickPlayTool.AutoCompact"); }
             set { EditorPrefs.SetBool("QuickPlayTool.AutoCompact", value); }
         }
 
+        // editor pref
         public static bool PresetsFoldout
         {
             get { return EditorPrefs.GetBool("QuickPlayTool.PresetsFoldout"); }
             set { EditorPrefs.SetBool("QuickPlayTool.PresetsFoldout", value); }
         }
 
-        public static void SetScenePresets(PresetsContainer presetContainer)
-        {
-            var json = JsonUtility.ToJson(presetContainer);
-            EditorPrefs.SetString("QuickPlayTool.Presets", json);
-        }
-
+        // editor pref
         public static bool CloseCurrentScenesOnPresetLoad
         {
             get { return EditorPrefs.GetBool("QuickPlayTool.CloseCurrentScenesOnPresetLoad"); }
             set { EditorPrefs.SetBool("QuickPlayTool.CloseCurrentScenesOnPresetLoad", value); }
         }
 
+
+
+
+
+        // PROJECT pref
+        public static string QuickPlaySceneRelativePath
+        {
+            get { return EditorPrefs.GetString("QuickPlayTool.QuickPlaySceneRelativePath"); }
+            set { EditorPrefs.SetString("QuickPlayTool.QuickPlaySceneRelativePath", value); }
+        }
+
+        // PROJECT pref
         public static PresetsContainer GetScenePresets()
         {
             if (!EditorPrefs.HasKey("QuickPlayTool.Presets"))
@@ -69,9 +79,24 @@ namespace QuickPlayTool
             }
         }
 
+        // PROJECT pref
+        public static void SetScenePresets(PresetsContainer presetContainer)
+        {
+            var json = JsonUtility.ToJson(presetContainer);
+            EditorPrefs.SetString("QuickPlayTool.Presets", json);
+        }
+
+
+
+
+
         public static string GetRawPresetJson()
         {
             return JsonHelper.FormatJson(EditorPrefs.GetString("QuickPlayTool.Presets"));
         }
+
+
+
+
     }
 }
